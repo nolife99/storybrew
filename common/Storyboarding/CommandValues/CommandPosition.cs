cd773ec30d8d9d63decb9419091216b8c2a5e57d
nolife99 <chendylan32@gmail.com>
2023-10-21 16:19:10 -0400

    added new conversion

--- common/Storyboarding/CommandValues/CommandPosition.cs
+++ common/Storyboarding/CommandValues/CommandPosition.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using System;
+using System.Drawing;
 
 namespace StorybrewCommon.Storyboarding.CommandValues
 {
@@ -61,7 +62,9 @@ namespace StorybrewCommon.Storyboarding.CommandValues
         public static bool operator !=(CommandPosition left, CommandPosition right) => !left.Equals(right);
         public static implicit operator OpenTK.Vector2(CommandPosition position) => new OpenTK.Vector2(position.x, position.Y);
         public static implicit operator Vector2(CommandPosition position) => new Vector2(position.x, position.Y);
+        public static implicit operator PointF(CommandPosition position) => new PointF(position.x, position.Y);
         public static implicit operator CommandPosition(OpenTK.Vector2 vector) => new CommandPosition(vector.X, vector.Y);
         public static implicit operator CommandPosition(Vector2 vector) => new CommandPosition(vector.X, vector.Y);
+        public static implicit operator CommandPosition(PointF vector) => new CommandPosition(vector.X, vector.Y);
     }
 }
